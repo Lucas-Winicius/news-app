52641ec4da2cdbff478d220e0a60a5b72df828c1
Abobora <lucasclucas7@gmail.com>
2023-01-21 00:00:40 -0300

    Container dos artigos iniciados

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <HeaderComponent />
+  <ArticleContainer 
+  titulo="OG Bitcoin Core Developer Claims Hack Drained Nearly All His BTC"
+  imagem="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/c8e3b3fe0595dfbab3656a5ba0e06e2c.jpg"
+  descricao="Even cryptocurrency’s most accomplished tech wizards apparently aren’t immune from the occasional wallet-draining hack. Luke Dashjr, one of the original core developers for Bitcoin, claims that someone swiped hundreds of BTC from his accounts late last year—l…"
+  autor="Lucas Ropek"
+  artigoOriginal="https://gizmodo.com/bitcoin-price-hack-217-btc-og-developer-luke-dashjr-1849944799"
+  data-de-publicacao="2023-01-03T20:48:00Z"
+  artigo-completo="Even cryptocurrencys most accomplished tech wizards apparently arent immune from the occasional wallet-draining hack. Luke Dashjr, one of the original core developers for Bitcoin, claims that someone… [+2723 chars]"
+  />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
+import ArticleContainer from './components/ArticleContainer.vue';
 
 export default {
   name: 'App',
-  components: { HeaderComponent }
+  components: { HeaderComponent, ArticleContainer }
 }
 </script>
 
---
+++ src/components/ArticleContainer.vue
@@ -0,0 +1,46 @@
+<template>
+  <article>
+    <div class="visualização" v-if="!estaAberto">
+      <h1>{{ titulo }}</h1>
+      <img :src="imagem" :alt="titulo">
+      <p>{{ descricao }}</p>
+      <small>{{ autor }}</small>
+    </div>
+    <div class="materiaCompleta" v-if="estaAberto">
+      <h1>{{ titulo }}</h1>
+      <img :src="imagem" :alt="titulo">
+      <p>{{ artigoCompleto }}</p>
+      <small>{{ autor }}</small>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+    name: 'ArticleContainer',
+    props: {
+        autor: String,
+        titulo: String,
+        descricao: String,
+        artigoOriginal: String,
+        imagem: String,
+        dataDePublicacao: String,
+        artigoCompleto: String,
+    },
+    data() { return { estaAberto: false } }
+}
+</script>
+
+<style>
+
+article {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.visualização {
+  width: 25vw;
+  
+}
+
+</style>
